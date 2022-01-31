752c50bc4fde0baec3e50c46247393cfff3d3f33
Taras Sidorenko <sidorenko.1992god@gmail.com>
2022-01-31 17:34:28 +0500

    upd

--- UI/src/App.js
+++ UI/src/App.js
@@ -4,8 +4,6 @@ import { store } from "./actions/store";
 import { Provider } from "react-redux";
 import { Container } from "@material-ui/core";
 import { ToastProvider } from "react-toast-notifications";
-import TRegs from './components/TRegs';
-import TQuestions from './components/TQuestions';
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Navbar/Sidebar";
 import {BrowserRouter} from "react-router-dom";
