e8cd469601d4d00ce18c7eec809eedfac4735f29
Taras Sidorenko <sidorenko.1992god@gmail.com>
2022-01-31 16:07:16 +0500

    first commit

---
+++ api/WebApplication1/WebApplication1/Controllers/RegController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public RegController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                       select regId,name,surname,middlename from reg";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("RegAppCon");
+            MySqlDataReader myReader;
+            using(MySqlConnection mycon=new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand myCommand=new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Reg rega)
+        {
+            string query = @"
+                        insert into reg (name,surname,middlename) values
+                                                    (@name,@surname,@middlename);
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("RegAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@name", rega.name);
+                    myCommand.Parameters.AddWithValue("@surname", rega.surname);
+                    myCommand.Parameters.AddWithValue("@middlename", rega.middlename);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Reg reg)
+        {
+            string query = @"
+                        update reg set 
+                        name =@name,
+                        surname=@surname,
+                        middlename=@middlename
+                        where regId=@regId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("RegAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@regId", reg.regId);
+                    myCommand.Parameters.AddWithValue("@name", reg.name);
+                    myCommand.Parameters.AddWithValue("@surname", reg.surname);
+                    myCommand.Parameters.AddWithValue("@middlename", reg.middlename);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        delete from reg 
+                        where regId=@regId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("RegAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@regId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Models/Reg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Reg
+    {
+        public int regId { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+        public string middlename { get; set; }
+    }
+}
